afa70288b9cd22b0bafef120756c8b5233678321
Raghav Dhingra <raghav.dhingra15@gmail.com>
2019-06-02 00:14:32 +0530

    Second Commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+import project1.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', project1.views.post_list, name='post_list'),
+    path('check/',project1.views.name, name='name'),
+    path('delete/',project1.views.delete, name='delete'),
 ]
--- project1/views.py
+++ project1/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+import django.contrib.sessions
 
 # Create your views here.
+def post_list(request):
+    return render(request, 'post_list.html',{})
+
+def name(request):
+    get_name = request.POST['text']
+    print(get_name)
+    request.session['name'] = get_name
+    print(request.session['name'])
+    return render(request,'output.html',{})
+
+def delete(request):
+    del request.session['name']
+    return render(request,'output.html',{})
